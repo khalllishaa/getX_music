22b5dbcff517b882a8c3f23b512274d06166a1c4
khalisha kaylanasywa <kkaylanasywa>
2024-10-08 07:21:46 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
+      initialRoute: '/splash',
       getPages: [
         GetPage(
           name: '/splash',
