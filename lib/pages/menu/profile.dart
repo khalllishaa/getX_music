03c46c540742ab8188a3515fbbdbefa3236a1c86
khalisha kaylanasywa <kkaylanasywa>
2024-10-14 13:14:25 +0700

    update

--- lib/pages/menu/profile.dart
+++ lib/pages/menu/profile.dart
@@ -4,69 +4,79 @@ import 'package:getx_music/reused/circle.dart';
 import 'package:getx_music/reused/color.dart';
 import 'package:getx_music/reused/songs.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
   const Profile({super.key});
 
   @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(height: 25),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 30.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 25),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    ProfilePicture(
-                      size: 180,
-                      imagePath: 'assets/profile.jpeg',
-                    ),
-                    SizedBox(width: 10),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    Row(
                       children: [
-                        Text(
-                          'Khal',
-                          style: TextStyle(
-                            color: colorWhite,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
+                        ProfilePicture(
+                          size: 130,
+                          imagePath: 'assets/profile.jpeg',
                         ),
-                        SizedBox(height: 5),
-                        Text(
-                          '4 Public Playlists • 21 Following',
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey,
-                          ),
-                        ),
-                        SizedBox(height: 10),
-                        Row(
+                        SizedBox(width: 10),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            ElevatedButton(
-                              onPressed: () {},
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: colorTransparent,
-                                side: BorderSide(color: colorFont, width: 1.0),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                              ),
-                              child: Text(
-                                'Edit',
-                                style: TextStyle(color: colorWhite),
+                            Text(
+                              'Khal',
+                              style: TextStyle(
+                                color: colorWhite,
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
-                            SizedBox(width: 10),
-                            IconButton(
-                              icon: Icon(Icons.more_vert, color: colorWhite),
-                              onPressed: () {},
+                            SizedBox(height: 5),
+                            Text(
+                              '4 Public Playlists • 21 Following',
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Row(
+                              children: [
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: colorTransparent,
+                                    side: BorderSide(color: colorFont, width: 1.0),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                                  ),
+                                  child: Text(
+                                    'Edit',
+                                    style: TextStyle(color: colorWhite),
+                                  ),
+                                ),
+                                SizedBox(width: 10),
+                                IconButton(
+                                  icon: Icon(Icons.more_vert, color: colorWhite),
+                                  onPressed: () {},
+                                ),
+                              ],
                             ),
                           ],
                         ),
@@ -74,128 +84,137 @@ class Profile extends StatelessWidget {
                     ),
                   ],
                 ),
-              ],
-            ),
+              ),
+              Text(
+                'Top tracks this month',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: colorWhite,
+                ),
+              ),
+              SizedBox(height: 10),
+              SongItem(
+                imagePath: 'assets/xxt.jpeg',
+                title: 'Everybody Dies In Their Nightmares',
+                subtitle: 'xxxtentacion',
+                size: 50,
+                onTap: () {},
+              ),
+              SongItem(
+                imagePath: 'assets/juice.jpeg',
+                title: 'All the girls are same',
+                subtitle: 'Juice WRLD',
+                size: 50,
+                onTap: () {},
+              ),
+              Text(
+                'Top artist this month',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: colorWhite,
+                ),
+              ),
+              SizedBox(height: 10),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+                        ProfilePicture(size: 110, imagePath: 'assets/xxt.jpeg'),
+                        SizedBox(height: 5),
+                        Text(
+                          'XXXTentacion',
+                          style: TextStyle(
+                            color: colorWhite,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 13,
+                          ),
+                        ),
+                        Text(
+                          'Artist',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 10,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 10),
+                    Column(
+                      children: [
+                        ProfilePicture(size: 110, imagePath: 'assets/juice.jpeg'),
+                        SizedBox(height: 5),
+                        Text(
+                          'Juice WRLD',
+                          style: TextStyle(
+                            color: colorWhite,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 13,
+                          ),
+                        ),
+                        Text(
+                          'Artist',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 10,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 10),
+                    Column(
+                      children: [
+                        ProfilePicture(size: 110, imagePath: 'assets/xxt.jpeg'),
+                        SizedBox(height: 5),
+                        Text(
+                          'XXXTentacion',
+                          style: TextStyle(
+                            color: colorWhite,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 13,
+                          ),
+                        ),
+                        Text(
+                          'Artist',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 10,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 10),
+                    Column(
+                      children: [
+                        ProfilePicture(size: 110, imagePath: 'assets/juice.jpeg'),
+                        SizedBox(height: 5),
+                        Text(
+                          'Juice Wrld',
+                          style: TextStyle(
+                            color: colorWhite,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 13,
+                          ),
+                        ),
+                        Text(
+                          'Artist',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 10,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+            ],
           ),
-          Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Top tracks this month',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: colorWhite,
-                  ),
-                ),
-                SizedBox(height: 10),
-                SongItem(
-                  imagePath: 'assets/xxt.jpeg',
-                  title: 'Everybody Dies In Their Nightmares',
-                  subtitle: 'xxxtentacion',
-                  size: 50, onTap: () {  },
-                ),
-                SongItem(
-                  imagePath: 'assets/juice.jpeg',
-                  title: 'All the girls are same',
-                  subtitle: 'Juice WRLD',
-                  size: 50, onTap: () {  },
-                ),
-              ],
-            ),
-          ),
-          Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Top artist this month',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: colorWhite,
-                  ),
-                ),
-                SizedBox(height: 10),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child:
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        children: [
-                          ProfilePicture(size: 130, imagePath: 'assets/xxt.jpeg'),
-                          SizedBox(height: 5),
-                          Text(
-                            'XXXTentacion',
-                            style: TextStyle(
-                              color: colorWhite,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 13,
-                            ),
-                          ),
-                          Text(
-                            'Artist',
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 10,
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(width: 10),
-                      Column(
-                        children: [
-                          ProfilePicture(size: 130, imagePath: 'assets/juice.jpeg'),
-                          SizedBox(height: 5),
-                          Text(
-                            'Juice WRLD',
-                            style: TextStyle(
-                              color: colorWhite,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 13,
-                            ),
-                          ),
-                          Text(
-                            'Artist',
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 10,
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(width: 10),
-                      Column(
-                        children: [
-                          ProfilePicture(size: 130, imagePath: 'assets/xxt.jpeg'),
-                          SizedBox(height: 5),
-                          Text(
-                            'XXXTentacion',
-                            style: TextStyle(
-                              color: colorWhite,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 13,
-                            ),
-                          ),
-                          Text(
-                            'Artist',
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 10,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
